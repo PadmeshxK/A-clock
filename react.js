4074c6ca4b8ce0530679530538b31f7d025c1891
PadmeshxK <hihello16112004@gmail.com>
2023-03-18 17:34:37 +0530

    update 1

---
+++ react.js
@@ -0,0 +1,33 @@
+const hourel = document.querySelector('.hour_arrow')
+const minutesel = document.querySelector('.minute_arrow')
+const secondsel = document.querySelector('.second_arrow')
+
+seconds_counter = 0
+minutes_counter = 0
+hours_counter = 0
+function update_clock(){
+    let hour_time = new Date().getHours()
+    const minute_time = new Date().getMinutes()
+    let second_time = new Date().getSeconds()
+    if (hour_time>12){
+        hour_time = hour_time - 12
+    }
+    if (second_time == 0){
+        seconds_counter += 60
+    }
+    if (minute_time == 0){
+        minutes_counter += 60
+    }
+    if (second_time == 0){
+        hours_counter += 60
+    }
+    second_time += seconds_counter
+    const rotation_deg_hour = (hour_time/12)*360
+    const rotation_deg_minute = (minute_time/60)*360
+    const rotation_deg_second = (second_time/60)*360
+    hourel.style.transform = `rotate(${rotation_deg_hour}deg)`
+    minutesel.style.transform = `rotate(${rotation_deg_minute}deg)`
+    secondsel.style.transform = `rotate(${rotation_deg_second}deg)`
+}
+
+setInterval(update_clock,1000)
